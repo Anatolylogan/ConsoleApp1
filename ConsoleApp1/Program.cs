2db9ee2f4891c3352ce1c199c215a09631781aa1
hom <hom@DESKTOP-F8TJ5BE>
2024-09-28 19:14:21 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{ 
+    static void Main()
+    {
+        Console.WriteLine("Введите ваш возраст");
+        string input = Console.ReadLine();
+        int age =int.Parse(input);
+        Console.WriteLine($"Ваш возраст: {age}");
+    }
+}
